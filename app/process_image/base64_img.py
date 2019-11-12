e877539ceb01ef351b52ea061332c8060e3acc35
gaborpelesz <gaborpelesz@hotmail.com>
2019-11-12 21:02:36 +0100

    Refactor comment

--- app/process_image/base64_img.py
+++ app/process_image/base64_img.py
@@ -3,13 +3,14 @@ import io
 from imageio import imread
 
 def base64_to_image(b64_string):
-    """Converting base64 string to cv2 image.
+    """Converting base64 string to numpy array
+        (in format of an image).
 
         Args:
             b64_string: Representing the base64 encoded image.
         
         Return:
-            A cv2 image, that we decoded from the base64 string,
+            A cv2 compatible image, that we decoded from the base64 string,
             stored in a numpy array.
     """
     # reconstruct image as a numpy array
