1a3d482a5c73a0e916e1d850b5ade18a2e1b3ef6
gaborpelesz <gaborpelesz@hotmail.com>
2019-11-12 20:50:07 +0100

    Debug model class

--- app/process_image/model.py
+++ app/process_image/model.py
@@ -1,14 +1,14 @@
 import json
 import numpy as np
+import sys
+from tensorflow.compat.v2.image import resize
 from tensorflow.keras.models import load_model
-from keras.applications.resnet50 import preprocess_input
-
-from process_image.base64_img import base64_to_image
+from tensorflow.keras.applications.resnet50 import preprocess_input
 
 class ClassifyImage:
     def __init__(self,  input_size=(224,224),
-                        model_path='model/object_classification.h5',
-                        label_path='model/labels.json'):
+                        model_path='app/model/object_classification.h5',
+                        label_path='app/model/labels.json'):
         self.model = load_model(model_path)
 
         # load labels
@@ -19,18 +19,17 @@ class ClassifyImage:
         self.labels = json_data
         self.input_size = input_size
 
-    def predict_base64(base64_str):
-        img = base64_to_image(base64_str)
-        img = tf.image.resize(np.array([img]), input_size)[0]
+    def predict(self, img):
+        img = np.array(resize(np.array([img]), self.input_size)[0])
 
         preprocessed_img = preprocess_input(img)
 
-        predictions = self.model.predict(np.array([x]))[0]
+        predictions = self.model.predict(np.array([preprocessed_img]))[0]
 
         highest_pred_index = np.where(predictions == max(predictions))[0][0]
 
         # map the correct label with the highest prediction's index
         for label, label_num in self.labels.items():
             if label_num == highest_pred_index:
-                return label_num
+                return (label, max(predictions))
         raise Exception("Couldn't find matching prediction index and label number...")
