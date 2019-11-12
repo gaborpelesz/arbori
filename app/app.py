87a7386ddff4cf9fff467b77ab9b6ec844dfca29
gaborpelesz <gaborpelesz@hotmail.com>
2019-11-12 20:55:01 +0100

    Refactor code

--- app/app.py
+++ app/app.py
@@ -10,7 +10,6 @@ model = ClassifyImage()
 
 @app.route("/predict", methods=["POST"])
 def predict():
-
     if request.method == "POST" and request.is_json:
         content = request.get_json()
 
@@ -23,17 +22,22 @@ def predict():
                 "error_message": str(e)
             }, status.HTTP_400_BAD_REQUEST
 
+        # if model is defined
         if model:
+            # save time for runtime calculation
             inference_start = time.time()
+
             img = base64_to_image(base64_image)
-            label, prob = model.predict(img)
-            print(label, prob)
+            label, probability = model.predict(img)
+
+            # save time for runtime calculation
             inference_end = time.time()
+            
             return json.dumps({
                     "status": "Success",
                     "prediction": {
                         "class": str(label),
-                        "probability": str(prob)
+                        "probability": str(probability)
                     },
                     "processing_runtime": '{:.3f}ms'.format((inference_end - inference_start)*1000)
                 })
@@ -42,7 +46,6 @@ def predict():
                 "status": "Failed",
                 "error_message": "The model wasn't initialized properly. There might be a bug in the code.",
             }, status.HTTP_500_INTERNAL_SERVER_ERROR
-        
         
 
 def main():
